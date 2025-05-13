c3ccdae5635166edb6103d83308e09dad4264283
Ingrid Maria <ingridmr857@gmail.com>
2025-05-12 21:20:46 -0300

    Ajustando pastas e subindo arquivo perdido. Ordem de deveres pela numeração da aula

---
+++ DeveresDeCasa/Dever-09/trainsize.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.4, random_state=42)
+
+print("Quantidade de itens na amostra de teste:", len(X_test))
